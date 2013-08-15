e8047eaf2026a428f8eb64efe733b208ef48ba89
Murat Saygili <murat.saygili@mni.thm.de>
2013-08-15 14:22:04 +0200

    First App Initialisation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeCup::Application.config.secret_key_base = 'ee395c9b3de4f17c4c9718eb1a0e999c0317c1e35851b62368be503b3b6bf5b2aa502e70ce61510825696c502e911b953f17663246feb6c96f42138e27f5b9c8'
