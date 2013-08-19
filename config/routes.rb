77c2f458048f0c9f4c56bd8e34f47b2860ae4e45
Murat Saygili <murat.saygili@mni.thm.de>
2013-08-19 21:09:10 +0200

    Benutzerverwaltung wurde angelegt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 TimeCup::Application.routes.draw do
 
-  devise_for :users
+  root :to =>'home#index'
+  devise_for :users,:controllers => { :registrations =>'registration'}
+  get 'dashboard' => 'home#dashboard'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
