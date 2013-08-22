5667c64c06c92bdb17b2a698a04daae1a088a7aa
Murat Saygili <Murat.Saygili@mni.thm.de>
2013-08-22 13:28:55 +0200

    Freunde einladen

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = current_user.events.all
   end
 
   # GET /events/1
@@ -14,11 +14,14 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-    @event = Event.new
+    @event = current_user.events.new
   end
 
   # GET /events/1/edit
   def edit
+  end
+
+  def invite
   end
 
 
@@ -26,7 +29,7 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.new(event_params)
 
     respond_to do |format|
       if @event.save
@@ -66,7 +69,7 @@ class EventsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
-      @event = Event.find(params[:id])
+      @event = current_user.events.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130820093517) do
+ActiveRecord::Schema.define(version: 20130822111241) do
 
   create_table "events", force: true do |t|
     t.string   "title"
     t.datetime "happend_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
